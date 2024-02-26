9d8fbcc04331b2cc868490fd3e49ae4b8dc29967
starmahmoodi <starmahmoodi@gmail.com>
2024-02-26 13:08:53 +0330

    66

--- functions.php
+++ functions.php
@@ -231,3 +231,18 @@ function setPostViews($postID) {
     }
 }
 add_filter('wpcf7_autop_or_not', '__return_false');
+function mytheme_add_woocommerce_support() {
+    add_theme_support( 'woocommerce', array(
+        'thumbnail_image_width' => 300,
+        'single_image_width'    => 600,
+        'product_grid'          => array(
+            'default_rows'    => 3,
+            'min_rows'        => 2,
+            'max_rows'        => 8,
+            'default_columns' => 4,
+            'min_columns'     => 2,
+            'max_columns'     => 5,
+        ),
+    ) );
+}
+add_action( 'after_setup_theme', 'mytheme_add_woocommerce_support' );
