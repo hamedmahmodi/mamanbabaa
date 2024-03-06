5d5c66d8691d283fc6b904ccb98dc06ce679934a
starmahmoodi <starmahmoodi@gmail.com>
2024-03-06 18:00:29 +0330

    222

--- js/form.js
+++ js/form.js
@@ -10,69 +10,202 @@ let relationship = document.getElementById("relationship");
 
 
 
-trueTell.addEventListener("change", function() {
-    if (this.checked) {
-        tellTwo.classList.add("show");
-    } 
-});
-falseTell.addEventListener("change", function() {
-    if (this.checked) {
-        tellTwo.classList.remove("show");
-    } 
-});
 
-anyOne.addEventListener("change", function() {
-    if (this.checked) {
-        fNameSomeOne.classList.add("show");
-        lNameSomeOne.classList.add("show");
-        relationship.classList.add("show");
-    } 
+// trueTell.addEventListener("change", function() {
+//     if (this.checked) {
+//         tellTwo.classList.add("show");
+//     } 
+// });
+// falseTell.addEventListener("change", function() {
+//     if (this.checked) {
+//         tellTwo.classList.remove("show");
+//     } 
+// });
+
+let addTell = document.getElementById("addTell");
+
+trueTell.addEventListener('change', function(){
+  if(this.checked){
+    addTell.innerHTML = '<input id="newTell" class="newTell" oninput="this.value = this.value.replace(/[^0-9]/g, \'\')" maxlength="11" aria-invalid="false" placeholder="شماره تماس" value="" type="tel" name="newTell">';
+  }
 });
-mySelf.addEventListener("change", function() {
-    if (this.checked) {
-        fNameSomeOne.classList.remove("show");
-        lNameSomeOne.classList.remove("show");
-        relationship.classList.remove("show");
-    } 
-});
-
-
-var currentPage = 1;
-showPage(currentPage);
-
-function nextPage(event) {
-  event.preventDefault();
-  if (validateInputs(currentPage)) {
-    if (currentPage < 3) {
-      currentPage++;
-      showPage(currentPage);
+falseTell.addEventListener('change', function(){
+  if(this.checked){
+    while (addTell.firstChild) {
+      addTell.removeChild(addTell.firstChild);
     }
   }
-}
+});
 
-function prevPage(event) {
-  event.preventDefault();
-  if (currentPage > 1) {
-    currentPage--;
-    showPage(currentPage);
+
+
+
+let registerAnyOne = document.getElementById("registerAnyOne");
+
+anyOne.addEventListener('change', function(){
+  if(this.checked){
+    registerAnyOne.innerHTML = '<input id="fNameSomeOne" class="fNameSomeOne" aria-invalid="false" placeholder="نام" value="" type="text" name="fNameSomeOne">';
+    registerAnyOne.innerHTML += '<input id="lNameSomeOne" class="lNameSomeOne" aria-invalid="false" placeholder="نام خانوادگی" value="" type="text" name="lNameSomeOne">';
+    registerAnyOne.innerHTML += '<input id="relationship" class="relationship" aria-invalid="false" placeholder="ارتباط شما با آن شخص" value="" type="text" name="relationship">';
   }
-}
-
-function showPage(page) {
-  var pages = document.getElementsByClassName("page");
-  for (var i = 1; i < pages.length; i++) {
-    pages[i].style.display = "none";
+});
+mySelf.addEventListener('change', function(){
+  if(this.checked){
+    while (registerAnyOne.hasChildNodes()) {
+      registerAnyOne.removeChild(registerAnyOne.firstChild);
+    }
+    
   }
-  document.getElementById("page" + page).style.display = "block";
-}
+});
 
-function validateInputs(page) {
-  var inputs = document.querySelectorAll("#page" + page + " input");
-  for (var i = 0; i < inputs.length; i++) {
-    if (inputs[i].value.trim() === "") {
-      alert("لطفاً تمامی فیلدها را پر کنید.");
-      return false;
+
+let page4 = document.getElementById("page4");
+let page5 = document.getElementById("page5");
+let page6 = document.getElementById("page6");
+
+document.getElementById("noAddPersonOne").addEventListener("change", function() {
+  if(this.checked) {
+    page4.style.display = "none";
+    page5.style.display = "none";
+    page6.setAttribute("id", "page4");
+  }
+});
+document.getElementById("addPersonOne").addEventListener("change", function() {
+  if(this.checked) {
+    page4.style.display = "block";
+    page5.style.display = "block";
+    page6.setAttribute("id", "page6");
+  }
+});
+document.getElementById("noAddPersonTwo").addEventListener("change", function() {
+  if(this.checked) {
+    page5.style.display = "none";
+      page6.setAttribute("id", "page5");
+  }
+});
+document.getElementById("addPersonTwo").addEventListener("change", function() {
+  if(this.checked) {
+    page5.style.display = "block" ;
+    page6.setAttribute("id", "page6");
+  }
+});
+
+
+
+
+let YourPlan = document.getElementById('YourPlan');
+
+document.getElementById('yesCallWeekends').addEventListener('change', function(){
+  if(this.checked){
+    YourPlan.innerHTML = 'برنامه شما: یک تماس در روز، دوشنبه تا جمعه به اضافه آخر هفته ها';
+  }
+});
+document.getElementById('noCallWeekends').addEventListener('change', function(){
+  if(this.checked){
+    YourPlan.innerHTML = 'برنامه شما: یک تماس در روز، دوشنبه تا جمعه';
+  }
+});
+
+
+
+
+let callTime = document.getElementById('callTime');
+let inputCount = 0;
+
+document.getElementById('oneCall').addEventListener('change', function(){
+  if(this.checked){
+    callTime.innerHTML = '<input type="time" name="appointment-time">';
+    inputCount = 1;
+  }
+  
+});
+
+document.getElementById('twoCall').addEventListener('change', function(){
+  if(this.checked){
+    if(inputCount < 2) {
+      callTime.innerHTML = '<input type="time" name="appointment-time">';
+      callTime.innerHTML += '<input type="time" name="appointment-timee">';
+    }
+    if(inputCount > 2){
+      
+      callTime.innerHTML = '<input type="time" name="appointment-time">';
+      callTime.innerHTML += '<input type="time" name="appointment-timee">';
+      
+    }
+    
+  }
+  inputCount=2;
+});
+document.getElementById('threeCall').addEventListener('change', function(){
+  if(this.checked){
+    if(inputCount < 3) {
+      callTime.innerHTML = '<input type="time" name="appointment-time">';
+      callTime.innerHTML += '<input type="time" name="appointment-timee">';
+      callTime.innerHTML += '<input type="time" name="appointment-timeee">';
+      
+    }
+    inputCount = 3;
+  }
+  
+});
+
+
+
+
+let ReferralCode = document.getElementById('ReferralCode');
+
+document.getElementById('yesReferralCode').addEventListener('change', function(){
+  if(this.checked){
+    ReferralCode.innerHTML = '<input type="text" name="ReferralCode">';
+  }
+});
+document.getElementById('noReferralCode').addEventListener('change', function(){
+  if(this.checked){
+    while (ReferralCode.firstChild) {
+      ReferralCode.removeChild(ReferralCode.firstChild);
     }
   }
-  return true;
-}
+});
+  
+
+
+// var currentPage = 1;
+// showPage(currentPage);
+
+// function nextPage(event) {
+//   event.preventDefault();
+//   if (validateInputs(currentPage)) {
+//     if (currentPage < 3) {
+//       currentPage++;
+//       showPage(currentPage);
+//     }
+//   }
+// }
+
+
+// function prevPage(event) {
+//   event.preventDefault();
+//   if (currentPage > 1) {
+//     currentPage--;
+//     showPage(currentPage);
+//   }
+// }
+
+// function showPage(page) {
+//   var pages = document.getElementsByClassName("page");
+//   for (var i = 1; i < pages.length; i++) {
+//     pages[i].style.display = "none";
+//   }
+//   document.getElementById("page" + page).style.display = "block";
+// }
+
+// function validateInputs(page) {
+//   var inputs = document.querySelectorAll("#page" + page + " input");
+//   for (var i = 0; i < inputs.length; i++) {
+//     if (inputs[i].value.trim() === "") {
+//       alert("لطفاً تمامی فیلدها را پر کنید.");
+//       return false;
+//     }
+//   }
+//   return true;
+// }
--- page-templates/service-template.php
+++ page-templates/service-template.php
@@ -7,125 +7,233 @@ get_header();
 
 <div class="servicePageBack">
     <div class="servicePage mainView">
-
+        <h1>ثبت نام تماس های مراقبت</h1>
 
         <div id="page1" class="page">
-        <h2>صفحه 1</h2>
-        <form>
-            <!-- فرم اطلاعات صفحه 1 -->
-            <input id="fName" class="fName" aria-invalid="false" placeholder="نام" value="" type="text" name="fName" required>
-            <input id="lName" class="lName" aria-invalid="false" placeholder="نام خانوادگی" value="" type="text" name="lName" required>
-            <input id=""email class="email" aria-invalid="false" placeholder="ایمیل" value="" type="email" name="email" required>
-            <input id="tell" class="tell" oninput="this.value = this.value.replace(/[^0-9]/g, '')" maxlength="11" aria-invalid="false" placeholder="شماره تماس" value="" type="tel" name="tell" required>
-            <input type="date" id="birthday" name="date"  required>
-            
-            <h3>آیا می خواهید خط تلفن دومی اضافه کنید؟</h3>
-            <span>
-                <label >بله</label>
-                <input type="radio" id="trueTell" name="tellTwo" value="true">
-            </span>
-            <span>
-                <label >خیر</label>
-                <input type="radio" id="falseTell" name="tellTwo" value="false">
-            </span>
+            <span>مرحله 1. اطلاعات مشتری</span>
+            <form>
+                <!-- فرم اطلاعات صفحه 1 -->
+                <div class="formRow">
+                    <input id="fName" class="fName" aria-invalid="false" placeholder="نام" value="" type="text" name="fName" required>
+                    <input id="lName" class="lName" aria-invalid="false" placeholder="نام خانوادگی" value="" type="text" name="lName" required>
+                </div>
+                <div class="formRow">
+                    <input id="email" class="email" aria-invalid="false" placeholder="ایمیل" value="" type="email" name="email" required>
+                    <input id="tell" class="tell" oninput="this.value = this.value.replace(/[^0-9]/g, '')" maxlength="11" aria-invalid="false" placeholder="شماره تماس" value="" type="tel" name="tell" required>
+                </div>
+                
+                <h2>آیا می خواهید خط تلفن دومی اضافه کنید؟</h2>
+                <span>
+                    <label >بله</label>
+                    <input type="radio" id="trueTell" name="tellTwo" value="true">
+                </span>
+                <span>
+                    <label >خیر</label>
+                    <input type="radio" id="falseTell" name="tellTwo" value="false">
+                </span>
+                <span id="addTell">
 
-            <!-- <input id="tellTwo" class="tellTwo" oninput="this.value = this.value.replace(/[^0-9]/g, '')" maxlength="11" aria-invalid="false" placeholder="شماره تماس" value="" type="tel" name="tellTwo"> -->
-            <textarea cols="40" rows="10" id="address" class="address" aria-required="true" aria-invalid="false" placeholder="آدرس" name="address" required></textarea>
+                </span>
 
-            <h3>آیا برای خود یا شخص دیگری ثبت نام می کنید؟</h3>
-            <span>
-                <label >خودم</label>
-                <input type="radio" id="mySelf" name="person" value="true">
-            </span>
-            <span>
-                <label >دیگری</label>
-                <input type="radio" id="anyOne" name="person" value="false">
-            </span>
-            
+                <input id="tellTwo" class="tellTwo" oninput="this.value = this.value.replace(/[^0-9]/g, '')" maxlength="11" aria-invalid="false" placeholder="شماره تماس" value="" type="tel" name="tellTwo">
+                <textarea cols="40" rows="10" id="address" class="address" aria-required="true" aria-invalid="false" placeholder="آدرس" name="address" required></textarea>
 
-            <!-- <input id="fNameSomeOne" class="fNameSomeOne" aria-invalid="false" placeholder="نام" value="" type="text" name="fNameSomeOne">
-            <input id="lNameSomeOne" class="lNameSomeOne" aria-invalid="false" placeholder="نام خانوادگی" value="" type="text" name="lNameSomeOne"> 
-            <input id="relationship" class="relationship" aria-invalid="false" placeholder="ارتباط شما با آن شخص" value="" type="text" name="relationship"> -->
-            <br>
-            <button onclick="nextPage(event)">صفحه بعدی</button>
-        </form>
+                <h2>آیا برای خود یا شخص دیگری ثبت نام می کنید؟</h2>
+                <span>
+                    <label >خودم</label>
+                    <input type="radio" id="mySelf" name="person" value="true">
+                </span>
+                <span>
+                    <label >دیگری</label>
+                    <input type="radio" id="anyOne" name="person" value="false">
+                </span>
+                <div id="registerAnyOne">
+
+                </div>
+                
+
+                <!-- <input id="fNameSomeOne" class="fNameSomeOne" aria-invalid="false" placeholder="نام" value="" type="text" name="fNameSomeOne">
+                <input id="lNameSomeOne" class="lNameSomeOne" aria-invalid="false" placeholder="نام خانوادگی" value="" type="text" name="lNameSomeOne"> 
+                <input id="relationship" class="relationship" aria-invalid="false" placeholder="ارتباط شما با آن شخص" value="" type="text" name="relationship"> -->
+                <br>
+                <input type="submit" value="صفحه بعدی" onclick="nextPage(event)">
+            </form>
         </div>
 
         <div id="page2" class="page">
-        <h2>صفحه 2</h2>
+        <span>مرحله 2. اطلاغات تکمیلی</span>
         <form>
             <!-- فرم اطلاعات صفحه 2 -->
-            <input id="fName" class="fName" aria-invalid="false" placeholder="نام" value="" type="text" name="fName" required>
-            <input id="lName" class="lName" aria-invalid="false" placeholder="نام خانوادگی" value="" type="text" name="lName" required>
-            <input id="tell" class="tell" oninput="this.value = this.value.replace(/[^0-9]/g, '')" maxlength="11" aria-invalid="false" placeholder="شماره تماس" value="" type="tel" name="tell" required>
-            <input type="date" id="birthday" name="date"  required>
-            
+            <textarea cols="40" rows="10" id="informationOne" class="address" aria-required="true" aria-invalid="false" placeholder="سلامت، دارو، یادآوری فعالیت" name="Reminders" required></textarea>
+            <textarea cols="40" rows="10" id="informationTwo" class="address" aria-required="true" aria-invalid="false" placeholder="سرگرمی ها، علایق، یادداشت ها" name="Hobbies" required></textarea>
+            <input type="date" id="birthday" name="birthday"  required>
 
-            <input id="tellTwo" class="tellTwo" oninput="this.value = this.value.replace(/[^0-9]/g, '')" maxlength="11" aria-invalid="false" placeholder="شماره تماس" value="" type="tel" name="tellTwo">
-            <textarea cols="40" rows="10" id="address" class="address" aria-required="true" aria-invalid="false" placeholder="آدرس" name="address" required></textarea>
-
-            <h3>آیا برای خود یا شخص دیگری ثبت نام می کنید؟</h3>
-            <span>
-                <label >خودم</label>
-                <input type="radio" id="mySelf" name="person" value="true" required>
-            </span>
-            <span>
-                <label >دیگری</label>
-                <input type="radio" id="anyOne" name="person" value="false" required>
-            </span>
-            
-
-            <input id="fNameSomeOne" class="fNameSomeOne" aria-invalid="false" placeholder="نام" value="" type="text" name="fNameSomeOne">
-            <input id="lNameSomeOne" class="lNameSomeOne" aria-invalid="false" placeholder="نام خانوادگی" value="" type="text" name="lNameSomeOne"> 
-            <input id="relationship" class="relationship" aria-invalid="false" placeholder="ارتباط شما با آن شخص" value="" type="text" name="relationship">
             <br>
-            <button onclick="prevPage(event)">صفحه قبلی</button>
-            <button onclick="nextPage(event)">صفحه بعدی</button>
+            <input type="submit" value="صفحه قبلی" onclick="nextPage(event)">
+            <input type="submit" value="صفحه بعدی" onclick="nextPage(event)">
         </form>
         </div>
 
         <div id="page3" class="page">
-        <h2>صفحه 3</h2>
-        <form>
-            <!-- فرم اطلاعات صفحه 3 -->
-            <input id="fName" class="fName" aria-invalid="false" placeholder="نام" value="" type="text" name="fName" required>
-            <input id="lName" class="lName" aria-invalid="false" placeholder="نام خانوادگی" value="" type="text" name="lName" required>
-            <input id="tell" class="tell" oninput="this.value = this.value.replace(/[^0-9]/g, '')" maxlength="11" aria-invalid="false" placeholder="شماره تماس" value="" type="tel" name="tell" required>
-            <input type="date" id="birthday" name="date"  required>
-            
-            <h3>آیا می خواهید خط تلفن دومی اضافه کنید؟</h3>
-            <span>
-                <label >بله</label>
-                <input type="radio" id="trueTell" name="tellTwo" value="true" required>
-            </span>
-            <span>
-                <label >خیر</label>
-                <input type="radio" id="falseTell" name="tellTwo" value="false" required>
-            </span>
+            <span>مرحله 3. اولویت تماس گیری</span>
+            <form>
+                <div class="formRow">
+                    <input id="fNamePersonOne" class="fName" aria-invalid="false" placeholder="نام" value="" type="text" name="fNamePersonOne" required>
+                    <input id="lNamePersonOne" class="lName" aria-invalid="false" placeholder="نام خانوادگی" value="" type="text" name="lNamePersonOne" required>
+                </div>
+                <div class="formRow">
+                    <input id="tellPersonOne" class="tell" oninput="this.value = this.value.replace(/[^0-9]/g, '')" maxlength="11" aria-invalid="false" placeholder="شماره تماس" value="" type="tel" name="tellPersonOne" required>
+                </div>
+                
+                <H2>روش ترجیحی ارتباط</H2>
+                <span>
+                    <label >تماس</label>
+                    <input type="radio" id="callPersonOne" name="call&messagePersonOne" required>
+                </span>
+                <span>
+                    <label >پیام</label>
+                    <input type="radio" id="messagePersonOne" name="call&messagePersonOne" required>
+                </span>
 
-            <input id="tellTwo" class="tellTwo" oninput="this.value = this.value.replace(/[^0-9]/g, '')" maxlength="11" aria-invalid="false" placeholder="شماره تماس" value="" type="tel" name="tellTwo">
-            <textarea cols="40" rows="10" id="address" class="address" aria-required="true" aria-invalid="false" placeholder="آدرس" name="address" required></textarea>
-
-            <h3>آیا برای خود یا شخص دیگری ثبت نام می کنید؟</h3>
-            <span>
-                <label >خودم</label>
-                <input type="radio" id="mySelf" name="person" value="true" required>
-            </span>
-            <span>
-                <label >دیگری</label>
-                <input type="radio" id="anyOne" name="person" value="false" required>
-            </span>
-            
-
-            <input id="fNameSomeOne" class="fNameSomeOne" aria-invalid="false" placeholder="نام" value="" type="text" name="fNameSomeOne">
-            <input id="lNameSomeOne" class="lNameSomeOne" aria-invalid="false" placeholder="نام خانوادگی" value="" type="text" name="lNameSomeOne"> 
-            <input id="relationship" class="relationship" aria-invalid="false" placeholder="ارتباط شما با آن شخص" value="" type="text" name="relationship">
-            <br>
-            <button onclick="prevPage(event)">صفحه قبلی</button>
-            <input class="submit" type="submit" value="ارسال">
-        </form>
+                <H2>آیا می خواهید یک مخاطب دوم اضافه کنید؟</H2>
+                <span>
+                    <label >بله</label>
+                    <input type="radio" id="addPersonOne" name="addPersonOne" value="true" required>
+                </span>
+                <span>
+                    <label >خیر</label>
+                    <input type="radio" id="noAddPersonOne" name="addPersonOne" value="false" required>
+                </span>
+                سحشد
+                <br>
+                <input type="submit" value="صفحه قبلی" onclick="nextPage(event)">
+                <input type="submit" value="صفحه بعدی" onclick="nextPage(event)">
+                
+            </form>
         </div>
+        <div id="page4" class="page">
+            <span>مرحله 3. اولویت تماس گیری</span>
+            <form>
+                <div class="formRow">
+                    <input id="fNamePersonTwo" class="fName" aria-invalid="false" placeholder="نام" value="" type="text" name="fNamePersonTwo" required>
+                    <input id="lNamePersonTwo" class="lName" aria-invalid="false" placeholder="نام خانوادگی" value="" type="text" name="lNamePersonTwo" required>
+                </div>
+                <div class="formRow">
+                    <input id="tellPersonTwo" class="tell" oninput="this.value = this.value.replace(/[^0-9]/g, '')" maxlength="11" aria-invalid="false" placeholder="شماره تماس" value="" type="tel" name="tellPersonTwo" required>
+                </div>
+                
+                <H2>روش ترجیحی ارتباط</H2>
+                <span>
+                    <label >تماس</label>
+                    <input type="radio" id="callPersonTwo" name="call&messagePersonTwo" required>
+                </span>
+                <span>
+                    <label >پیام</label>
+                    <input type="radio" id="messagePersonTwo" name="call&messagePersonTwo" required>
+                </span>
 
+                <H2>آیا می خواهید یک مخاطب سوم اضافه کنید؟</H2>
+                <span>
+                    <label >بله</label>
+                    <input type="radio" id="addPersonTwo" name="addPersonTwo" value="true" required>
+                </span>
+                <span>
+                    <label >خیر</label>
+                    <input type="radio" id="noAddPersonTwo" name="addPersonTwo" value="false" required>
+                </span>
+            
+                <br>
+                <input type="submit" value="صفحه قبلی" onclick="nextPage(event)">
+                <input type="submit" value="صفحه بعدی" onclick="nextPage(event)">
+                
+            </form>
+        </div>
+        <div id="page5" class="page">
+            <span>مرحله 3. اولویت تماس گیری</span>
+            <form>
+                <div class="formRow">
+                    <input id="fNamePersonThree" class="fName" aria-invalid="false" placeholder="نام" value="" type="text" name="fNamePersonThree" required>
+                    <input id="lNamePersonThree" class="lName" aria-invalid="false" placeholder="نام خانوادگی" value="" type="text" name="lNamePersonThree" required>
+                </div>
+                <div class="formRow">
+                    <input id="tellPersonThree" class="tell" oninput="this.value = this.value.replace(/[^0-9]/g, '')" maxlength="11" aria-invalid="false" placeholder="شماره تماس" value="" type="tel" name="tellPersonThree" required>
+                </div>
+                
+                <H2>روش ترجیحی ارتباط</H2>
+                <span>
+                    <label >تماس</label>
+                    <input type="radio" id="callPersonThree" name="call&messagePersonThree" required>
+                </span>
+                <span>
+                    <label >پیام</label>
+                    <input type="radio" id="messagePersonThree" name="call&messagePersonThree" required>
+                </span>
+            
+                <br>
+                <input type="submit" value="صفحه قبلی" onclick="nextPage(event)">
+                <input type="submit" value="صفحه بعدی" onclick="nextPage(event)">
+                
+            </form>
+        </div>
+        <div id="page6" class="page" >
+            <span>مرحله 4. طرح خود را انتخاب کنید</span>
+            <form>
+                <h2>تعداد تماس در طول روز</h2>
+                <span>
+                    <label >یک</label>
+                    <input type="radio" id="oneCall" name="manyCallDay" required>
+                </span>
+                <span>
+                    <label >دو</label>
+                    <input type="radio" id="twoCall" name="manyCallDay" required>
+                </span>
+                <span>
+                    <label >سه</label>
+                    <input type="radio" id="threeCall" name="manyCallDay" required>
+                </span>
+
+                <h2>آیا می خواهید آخر هفته ها با شما تماس بگیرند؟</h2>
+                <span>
+                    <label >بله</label>
+                    <input type="radio" id="yesCallWeekends" name="callWeekends" required>
+                </span>
+                <span>
+                    <label >خیر</label>
+                    <input type="radio" id="noCallWeekends" name="callWeekends" required>
+                </span>
+                <span id="YourPlan"></span>
+            </form>
+            <span>مرحله 5. دوست دارید چه ساعتی با شما تماس بگیرند؟</span>
+            <form>
+                <div id="callTime" class="callTime">
+                    
+                </div>
+                <h2>آیا باید در زمان دقیق یا بعد از ساعت کاری تماس بگیرید؟</h2>
+                <span>
+                    <label >بله</label>
+                    <input type="radio" id="yesExactTime" name="exactTime" required>
+                </span>
+                <span>
+                    <label >خیر</label>
+                    <input type="radio" id="noExactTime" name="exactTime" required>
+                </span>
+                
+                <h2>در نهایت، آیا کد تخفیف یا معرفی دارید؟</h2>
+                <span>
+                    <label >بله</label>
+                    <input type="radio" id="yesReferralCode" name="referralCode" required>
+                </span>
+                <span>
+                    <label >خیر</label>
+                    <input type="radio" id="noReferralCode" name="referralCode" required>
+                </span>
+                <span id="ReferralCode">
+
+                </span>
+            </form>
+        </div>
+        
+        <input class="submit" type="submit" value="ارسال">
     </div>
 <?php
 get_footer();
